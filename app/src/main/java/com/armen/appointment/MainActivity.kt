b6ff65cb570fb7e343e852733cc24eb2f3c10409
aapresyan <armenapresyan5@gmail.com>
2022-02-16 01:58:52 +0400

    add text

--- app/src/main/java/com/armen/appointment/MainActivity.kt
+++ app/src/main/java/com/armen/appointment/MainActivity.kt
@@ -3,13 +3,19 @@ package com.armen.appointment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.armen.appointment.ui.theme.AppointmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppointmentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                listOf(
+                                    Color.Blue,
+                                    Color(0xFFCC00CC)
+                                )
+                            )
+                        )
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = "Doctor",
+                        color = Color.White,
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(start = 32.dp, top = 64.dp)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppointmentTheme {
-        Greeting("Android")
     }
 }
